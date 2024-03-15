359957767d44b1f5d53595f0fcfc81906ba762e3
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-03-15 18:48:39 -0300

    Set(Add): Swagger on API

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+    
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
