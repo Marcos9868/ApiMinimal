e83c63e589e3937cd9157998e9d9f2eabeae2b75
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-03-19 14:34:16 -0300

    Feat(Add): JsonIgnore on unused params

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,10 +1,14 @@
+using System.Text.Json.Serialization;
+
 namespace ApiMinimal.Models
 {
     public class Category
     {
+        [JsonIgnore]
         public int Id { get; set; } = 0;
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        [JsonIgnore]
         public ICollection<Product>? Products { get; set; }
     }
 }
